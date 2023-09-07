4683cab860a17bfe56248b3b2abafb544d191c99
Nicolas Herrera <herreran903@gmail.com>
2023-09-07 18:54:19 -0500

    Se creo la funcion invert

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -4,3 +4,14 @@
 ;; Nicolas Herrera 2182551
 
 #lang eopl
+
+;Invierte los elementos de los pares dentro de una lista
+;invert : List -> List
+;usage: (list-length l) = Invierte los pares de L
+(define invert
+  (lambda(L)
+    (if (null? L)
+    '()
+    (cons
+     (list (cadr(car L)) (car(car L)))
+     (invert(cdr L))))))
