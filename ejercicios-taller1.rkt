77fd5499c00c5bc03a5b9d37b4ca91e45e86d204
Nicolas Herrera <herreran903@gmail.com>
2023-09-07 21:39:55 -0500

    Se creo la funcion filter-in

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -15,3 +15,14 @@
     (cons
      (list (cadr(car L)) (car(car L)))
      (invert(cdr L))))))
+
+;Retorna una lista que contiene los elementos que pertenecen a L y que satisfacen el predicado P
+;filter-in : List -> List
+;usage: (list-length l) = Lista de los elementos que cumplen P
+(define filter-in
+  (lambda (P L)
+    (if (null? L)
+        '()
+        (if (P (car L))
+            (cons (car L) (filter-in P (cdr L)))
+            (filter-in P (cdr L))))))
