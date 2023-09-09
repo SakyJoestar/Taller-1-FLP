567b4505e5cbbf72aaaeb5223689c92860449b66
Nicolas Herrera <herreran903@gmail.com>
2023-09-09 11:33:21 -0500

    Se creo la funcion Operar-binarias y se cambiaron los comentarios

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -5,9 +5,14 @@
 
 #lang eopl
 
-;Invierte los elementos de los pares dentro de una lista
-;invert : List -> List
-;usage: (list-length l) = Invierte los pares de L
+;; invert:
+;; Proposito:
+;; L -> L' : Invierte los pares de L.
+;;
+;; <lista> ::= () | (<par> <lista>)
+;; <par>   ::= (<elemento> <elemento>)
+;; <elemento>   ::= <number> | <symbol> | <string>
+
 (define invert
   (lambda(L)
     (if (null? L)
@@ -16,9 +21,18 @@
      (list (cadr(car L)) (car(car L)))
      (invert(cdr L))))))
 
-;Retorna una lista que contiene los elementos que pertenecen a L y que satisfacen el predicado P
-;filter-in : List -> List
-;usage: (list-length l) = Lista de los elementos que cumplen P
+;; Pruebas
+(invert '((a 1) (a 2) (1 b) (2 b)))
+(invert '((5 9) (10 91) (82 7) (a e) ("hola" "Mundo")))
+(invert '(("es" "racket") ("genial" "muy") (17 29) (81 o)))
+
+;; filter-in:
+;; Proposito:
+;; P x L -> L' : Retorna una lista que contiene los elementos que pertenecen a L y que satisfacen el predicado P
+;;
+;; <lista> ::= () | (<elemento> <lista>)
+;; <elemento>   ::= <number> | <symbol> | <string>
+
 (define filter-in
   (lambda (P L)
     (if (null? L)
@@ -27,12 +41,18 @@
             (cons (car L) (filter-in P (cdr L)))
             (filter-in P (cdr L))))))
 
+;; Pruebas
+(filter-in number? '(a 2 (1 3) b 7))
+(filter-in symbol? '(a (b c) 17 foo))
+(filter-in string? '(a b u "univalle" "racket" "flp" 28 90 (1 2 3)))
 
+;; cartesian-product
+;; Proposito:
+;; L1 x L2 -> L' : Retorna una L' de tuplas que representen el producto cartesiano entre L1 y L2.
+;;
+;; <lista> ::= () | (<elemento> <lista>)
+;; <elemento>   ::= <number> | <symbol> | <string>
 
-
-;Retorna una lista de tuplas que representen el producto cartesiano entre L1 y L2. 
-;cartesian-product : List -> List
-;usage: (list-length l) = Lista del producto cartesiano entre L1 Y L2
 (define cartesian-product
   (lambda (L1 L2)
     (if (null? L1)
@@ -43,11 +63,46 @@
                   (append (cartesian-product (list (car L1)) (cdr L2))
                           (cartesian-product (cdr L1) L2)))))))
 
-;Retorna una lista donde la posicion n-esima corresponde al resultado de aplicar la funcion F sobre los elementos en la posicion n-esima en L1 y L2.
-;cartesian-product : List -> List
-;usage: (list-length l) = Lista de aplicar F entre los elementos de L1 Y L2
+;; Pruebas
+(cartesian-product '(a b c) '(x y))
+(cartesian-product '(p q r) '(5 6 7))
+
+;; cartesian-product
+;; Proposito:
+;; F x L1 x L2 -> L' : Retorna una L' donde la posicion n-esima corresponde al resultado de aplicar la funcion F sobre los elementos en la posicion n-esima en L1 y L2.
+;;
+;; <lista> ::= () | (<elemento> <lista>)
+;; <elemento>   ::= <number> | <symbol> | <string>
+
 (define zip
   (lambda (F L1 L2)
     (if (null? L1)
         '()
         (cons (F (car L1)(car L2))(zip F (cdr L1)(cdr L2))))))
+
+;; Pruebas
+(zip + '(1 4) '(6 2))
+(zip * '(11 5 6) '(10 9 8))
+
+;; cartesian-product
+;; Proposito:
+;; OperacionB -> int : Retorna el int de hacer operaciones suma, resta y multiplicacion correspondientes segun la OperacionB.
+;;
+;; <OperacionB> ::= <int>
+;;              ::= <OperacionB> ’suma <OperacionB>)
+;;              ::= <OperacionB> ’resta <OperacionB>)
+;;              ::= <OperacionB> ’multiplica <OperacionB>)
+(define Operar-binarias
+  (lambda (OperacionB)
+    (if(number? OperacionB)
+       OperacionB
+       (cond [(eqv? 'suma (cadr OperacionB)) (+ (Operar-binarias(car OperacionB))(Operar-binarias(caddr OperacionB)))]
+             [(eqv? 'resta (cadr OperacionB)) (- (Operar-binarias(car OperacionB))(Operar-binarias(caddr OperacionB)))]
+             [(eqv? 'multiplica (cadr OperacionB)) (* (Operar-binarias(car OperacionB))(Operar-binarias(caddr OperacionB)))]))))
+
+;; Pruebas
+(Operar-binarias 4)
+(Operar-binarias '(2 suma 9))
+(Operar-binarias '(2 multiplica 9))
+(Operar-binarias '((2 multiplica 3)suma(5 resta 1 )))
+(Operar-binarias '((2 multiplica(4 suma 1))multiplica((2 multiplica 4)resta 1)))
