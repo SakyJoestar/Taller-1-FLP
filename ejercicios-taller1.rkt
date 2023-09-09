d544c7bf05a7cbe88bd7061dc3bca1a5b8755a15
Nicolas Herrera <herreran903@gmail.com>
2023-09-09 08:44:25 -0500

    Se creo la funcion cartesian-product

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -26,3 +26,19 @@
         (if (P (car L))
             (cons (car L) (filter-in P (cdr L)))
             (filter-in P (cdr L))))))
+
+
+
+
+;Retorna una lista de tuplas que representen el producto cartesiano entre L1 y L2. 
+;cartesian-product : List -> List
+;usage: (list-length l) = Lista del producto cartesiano entre L1 Y L2
+(define cartesian-product
+  (lambda (L1 L2)
+    (if (null? L1)
+        '()
+        (if (null? L2)
+            '()
+            (cons (list (car L1) (car L2))
+                  (append (cartesian-product (list (car L1)) (cdr L2))
+                          (cartesian-product (cdr L1) L2)))))))
